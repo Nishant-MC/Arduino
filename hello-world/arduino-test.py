e4df60f335068cc7640d2a2fac3f2ad0c25a5df8
Nishant Mohanchandra <nm1345@nyu.edu>
2015-09-13 20:20:28 +0800

    Updated .py test code
    .py code now broadcasts to all available serial ports and works for
    Linux and Windows (possible to do: make it work for OSX)
    
    Still need to succeed with getting a working virtual COM port

--- hello-world/arduino-test.py
+++ hello-world/arduino-test.py
@@ -1,31 +1,66 @@
-# Python's interface for serial port communication
+import glob
 import serial
+import sys
 
-# Send a byte 'val' as serial input to the Arduino (assumes prior connection)
-def send_arduino(connection,val):
-    connection.write(val)
 
-# Arduino communicates on COM3 @ 9600 bps
-PORT = 'COM3'
-SPEED = 9600
+# Creates a list of all serial ports to broadcast to (cross-platform)
+def make_serial_ports_list():
+    
+    # On Windows, serial ports start with the prefix "COM"
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
 
+    # On Linux / Cygwin, serial ports start with the prefix /dev/tty    
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+        
+    # Don't know how to work with OSX / other OS types as of now
+    else:
+        raise EnvironmentError('Unsupported platform / OS')
+
+    # Try connecting to ports; any open ones are appended to the result
+    # All closed/invalid ports are left alone
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+        
+    return result
+
+# Send byte 'val' as input to all serial ports (assumes prior connections)
+def send_serial(connections,val):
+    for c in connections:
+        c.write(val)
+
+# Main code
 if __name__ == '__main__':
-    # Establish connection with the Arduino
-    connection = serial.Serial( PORT, 
-                                SPEED,
-                                timeout=0,
-                                stopbits=serial.STOPBITS_TWO )
 
-    # Sending byte inputs to the Arduino
+    # Make all connections @ 9600 bps (Arduino emulation)
+    CONNECTIONS = []
+    PORTS = make_serial_ports_list()
+    SPEED = 9600
+
+    for PORT in PORTS:
+        CONNECTIONS.append( serial.Serial( PORT, SPEED,
+                                           timeout=0,
+                                           stopbits=serial.STOPBITS_TWO ) )
+
+    print("Broadcasting to ports: ", PORTS)
+
+    # Sending byte inputs to available serial ports
     choice = 'y'
     while choice == 'y' or choice == 'n':
-        choice = input("y -> LED on, n -> LED off, q -> LED on & quit: ")
+        choice = input("y -> send '1', n -> send '0', q -> send '1' & quit: ")
         if choice == 'y':
-            send_arduino(connection,b'1')
+            send_serial(CONNECTIONS, b'1')
         elif choice == 'n':
-            send_arduino(connection,b'0')
+            send_serial(CONNECTIONS, b'0')
         elif choice == 'q':
-            send_arduino(connection,b'1')
+            send_serial(CONNECTIONS, b'1')
             break
         else:
             print("Undefined choice... try again.")
@@ -33,5 +68,6 @@ if __name__ == '__main__':
             continue
 
 
-    # Close the connection when done
-    connection.close()
+    # Close the connections when done
+    for c in CONNECTIONS:
+        c.close()
